31a1b060ffdde4f3c1ca9a815af8ffdbfdac178b
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-26 11:15:35 +0000

    hehe

---
+++ 01 basics/03_datatypeconvo.js
@@ -0,0 +1,19 @@
+let score= "33"
+console.log(typeof score);
+let numberInValue =Number(score)
+console.log(typeof score);
+console.log(numberInValue);
+
+let inLoggedIn = "rishi"
+console.log(inLoggedIn);
+let  BooleaninLoggedIn = Boolean(inLoggedIn)
+console.log(BooleaninLoggedIn);
+console.log(typeof BooleaninLoggedIn);
+
+
+
+
+
+let Value=3
+let negValue=-Value 
+console.log(negValue)
