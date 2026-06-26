4b3967a4eac1a84bbbead72031bd71e74676bc4e
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-26 11:16:18 +0000

    lerned

---
+++ 01 basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId= 14452
+let accountEmail="rishita@gmail.com"
+var accountPassword="27yuhe"
+accountCity="jaipur"
+
+//accountId=2
+accountPassword="dwdiw"
+accountCity="uttarakhand"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
