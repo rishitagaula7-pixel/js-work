5de934252d83b1c356a4bbda927ac1c226ce3c51
Rishita Gaula <rishitagaula7@gmail.com>
2026-06-27 13:37:20 +0000

    done

---
+++ 01 basics/07_numbers.js
@@ -0,0 +1,13 @@
+const number = 23.9876
+console.log(number.toPrecision(5));
+
+const newNum = 10092832843546567;// india no. system
+console.log(newNum.toLocaleString('en-IN'));
+console.log(newNum.toLocaleString());//comma aajayenge international
+
+
+
+console.log(Math.round(9.7));
+console.log(Math.ceil(4.2));//isese bdi wali round off value
+console.log(Math.floor(4.2));//isese choti
+console.log(Math.min(3 ,4 ,7 , 2));
