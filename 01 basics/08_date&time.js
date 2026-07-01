00348080f34a2be796840b9283f91ffc8accc15a
Rishita Gaula <rishitagaula7@gmail.com>
2026-07-01 10:54:52 +0000

    done

---
+++ 01 basics/08_date&time.js
@@ -0,0 +1,30 @@
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+
+
+
+// let myCreatedDate = new Date(2026,6,28);
+// console.log(myCreatedDate.toDateString());
+// console.log(myCreatedDate.toLocaleDateString());
+
+
+
+
+
+
+// let myTimeStamp = Date.now();
+// console.log(myCreatedDate.getTime());
+// console.log(Date.now())
+
+let newDate= new Date();
+console.log(newDate.getMonth()+1);//kyunki js mein months 0 se strt hote hai
+
+
+
+
+// newDate=toLocaleString('default'{weekDay="Long" })
